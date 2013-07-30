48dd7bc3ebc8b921bf778dfcfe0c4f9ee42f56cb
pengshanming <765614247@qq.com>
2013-07-30 11:44:30 +0800

    sea-76

---
+++ console/app/my/finder/console/service/MyMongoManager.scala
@@ -0,0 +1,28 @@
+package my.finder.console.service
+
+
+import play.api.Play.current
+import com.mongodb.casbah.Imports._
+
+/**
+ *
+ */
+object MyMongoManager {
+
+  var mongoClient:MongoClient = null
+  val myMongoDBPort = current.configuration.getInt("myMongoDBPort").get
+  val myMongoDBUrl = current.configuration.getString("myMongoDBUrl").get
+  def apply():MongoClient = {
+    synchronized{
+      if (mongoClient == null) {
+        mongoClient = MongoClient(myMongoDBUrl,myMongoDBPort)
+      }
+      mongoClient
+    }
+  }
+  /*def coleDB(mongoClient:MongoClient ) ={
+        if( mongoClient != null ){
+          mongoClient.close()
+        }
+  }*/
+}
