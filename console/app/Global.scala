1f7a490b2e26b0e6955510551df3008e120ebabf
cheb2003 <amulo2003@sina.com>
2013-08-05 14:32:41 -0400

    SEA-91 自定义ActorSystem不能重启问题

--- console/app/Global.scala
+++ console/app/Global.scala
@@ -1,62 +1,37 @@
 
-import akka.actor.{ ActorSystem, Props}
-import com.typesafe.config.ConfigFactory
 
-import my.finder.console.actor.{MessageFacade, ConsoleRootActor}
-import my.finder.console.service.{DBMysql, InitJob, GenarateSearchOrderPerDay, IndexManage}
-import my.finder.common.util._
+import my.finder.console.actor.MessageFacade
+import my.finder.console.service.{DBMysql, InitJob, IndexManage}
 
 import org.quartz.impl.StdSchedulerFactory
-import org.quartz.{Scheduler, JobBuilder}
+import org.quartz.Scheduler
 import play.api._
-import play.api.Play.current
+
 /**
  *
  */
 object Global extends GlobalSettings {
 
-  //private val system:ActorSystem = getActorSystem
-  private var system:ActorSystem = null
-  private var scheduler:Scheduler = null
+  private var scheduler: Scheduler = null
+
   override def onStart(app: Application) {
-    synchronized{
+    synchronized {
       IndexManage.init
       DBMysql.init
-      /*system = getActorSystem
-      val root = system.actorOf(Props[ConsoleRootActor], "root")
-      if (root != null) {
-        Logger.debug("console actor system has started")
-        MessageFacade.rootActor = root
-      } else {
-        Logger.debug("console actor system has not started")
-        throw new RuntimeException("console actor system has not started")
-      }*/
-
     }
-    scheduler = StdSchedulerFactory.getDefaultScheduler()
+    scheduler = StdSchedulerFactory.getDefaultScheduler
     scheduler.start()
     InitJob.init(scheduler)
   }
 
   override def onStop(app: Application) {
     synchronized {
-      if (system != null) {
-        Logger.debug("console actor system has shutdown")
-        system.shutdown()
-      }
-      if(scheduler != null){
+      MessageFacade.shutdown()
+      if (scheduler != null) {
         scheduler.shutdown()
       }
     }
   }
 
-  private def getActorSystem : ActorSystem = {
-    //println("-----" + current.configuration.getString("profile").get)
-    if(Util.getProfile == Constants.PROFILE_PRODUCTION)
-      ActorSystem.create("console", ConfigFactory.load().getConfig("console." + Constants.PROFILE_PRODUCTION))
-    else if(Util.getProfile == Constants.PROFILE_TEST)
-      ActorSystem.create("console", ConfigFactory.load().getConfig("console." + Constants.PROFILE_TEST))
-    else       
-      null
-  }
+
 }
--- console/app/my/finder/console/actor/MessageFacade.scala
+++ console/app/my/finder/console/actor/MessageFacade.scala
@@ -1,11 +1,37 @@
 package my.finder.console.actor
 
-import akka.actor.{ActorRef}
+import akka.actor.{ActorSystem, Props, ActorRef}
+import my.finder.common.util.{Constants, Util}
+import com.typesafe.config.ConfigFactory
 
 /**
  *
  */
 
 object MessageFacade {
-  var rootActor: ActorRef = null
+  private var root: ActorRef = null
+  private var system:ActorSystem = null
+
+  def rootActor: ActorRef = {
+    if(root == null){
+      system = getActorSystem
+      root = system.actorOf(Props[ConsoleRootActor], "root")
+    }
+    root
+  }
+
+  private def getActorSystem : ActorSystem = {
+    if(Util.getProfile == Constants.PROFILE_PRODUCTION)
+      ActorSystem.create("console", ConfigFactory.load().getConfig("console." + Constants.PROFILE_PRODUCTION))
+    else if(Util.getProfile == Constants.PROFILE_TEST)
+      ActorSystem.create("console", ConfigFactory.load().getConfig("console." + Constants.PROFILE_TEST))
+    else
+      null
+  }
+
+  def shutdown() = {
+    if(system != null){
+      system.shutdown()
+    }
+  }
 }
