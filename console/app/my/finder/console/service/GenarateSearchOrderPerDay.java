e9f22362635f0fb8ee55fbe1880e29661bbc6662
pengshanming <765614247@qq.com>
2013-08-15 10:19:21 +0800

    day-15

--- console/app/my/finder/console/service/GenarateSearchOrderPerDay.java
+++ console/app/my/finder/console/service/GenarateSearchOrderPerDay.java
@@ -11,10 +11,12 @@ import java.util.Date;
 public class GenarateSearchOrderPerDay implements Job {
     public void execute(JobExecutionContext context)
             throws JobExecutionException {
+
         java.util.Calendar calend = java.util.Calendar.getInstance();
         calend.setTime(context.getFireTime());
         calend.add(java.util.Calendar.DATE, -1);
         KPIService.paymentOrder(calend);
+        KPIService.SearchNoResult(calend);
     }
 
 }
